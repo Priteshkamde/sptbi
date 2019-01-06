68a3437253830767f7280fc3af32df42d4dffefb
rojagtap <rohanj30@icloud.com>
2019-01-06 11:33:13 +0530

    none

--- sptbi/settings.py
+++ sptbi/settings.py
@@ -136,8 +136,8 @@ MEDIA_URL = '/media/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rohan.jagtap@spit.ac.in'
-EMAIL_HOST_PASSWORD = 'nigganigganigga'
+EMAIL_HOST_USER = '<email here>'
+EMAIL_HOST_PASSWORD = '<password here>'
 EMAIL_PORT = 587
 
 
